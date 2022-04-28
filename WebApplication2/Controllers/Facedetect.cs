af06ca240f1141187d3735ed10b7cff7e0bbb08c
Danil <Save_ae72@student.itstep.org>
2022-04-28 23:18:17 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/Facedetect.cs
@@ -0,0 +1,38 @@
+﻿using Google.Cloud.Vision.V1;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FaceDetect : ControllerBase
+    {
+
+        private readonly ILogger<FaceDetect> _logger;
+
+        public FaceDetect(ILogger<FaceDetect> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string GetFaceCoord(string url)
+        {
+            string poly = String.Empty;
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "key.json");
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            IReadOnlyList<FaceAnnotation> result = client.DetectFaces(Image.FromUri(url));
+            foreach (FaceAnnotation face in result)
+            {
+                poly = string.Join(" - ", face.BoundingPoly.Vertices.Select(v => $"({v.X}, {v.Y})"));
+
+            }
+            return poly;
+        }
+    }
+}
